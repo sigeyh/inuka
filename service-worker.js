4490c4b0eb1f18bb69e019b1025052d5fc966d67
sigeyh <rsigeyh28@gmail.com>
2025-03-27 13:13:58 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'landing-page-cache-v1';
+const urlsToCache = [
+  'index.html',
+  'styles.css',
+  'logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
